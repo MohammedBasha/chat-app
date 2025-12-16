f2f30581a4acc8043f0c388034ec8e3e285b71d8
MohammedBasha <me@mohammedbasha.com>
2025-12-16 20:46:43 +0200

    Adding the user routes and related functionality

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
+
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
@@ -11,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware to parse JSON requests body (req.body)
 app.use(express.json());
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
     // root route http://localhost:${PORT}/
@@ -19,6 +25,8 @@ app.get("/", (req, res) => {
 
 // Creating the auth routes
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
     connectToMongoDB();
